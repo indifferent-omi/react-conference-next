ee626375cc2fa0e37e3ac6fde86a167b93c4e872
Abdullah khalid omi <a.khalidomi@gmail.com>
2022-09-20 00:19:05 +0600

    build fix

--- src/components/ScheduleDetails/SpeakerContent.jsx
+++ src/components/ScheduleDetails/SpeakerContent.jsx
@@ -3,11 +3,11 @@ import UserCard from '../Cards/UserCard';
 const SpeakerContent = ({ data }) =>{
     
     return(
-        <>
+        <div>
         {
            data && data.map(el => <UserCard data={el} key={el.id} className="mb-10"/>)
         }
-        </>
+        </div>
     )
 }
 
