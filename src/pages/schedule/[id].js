5f40abd6e53a768cd1e38f4e10a5e80181256466
Abdullah khalid omi <a.khalidomi@gmail.com>
2022-09-20 00:44:24 +0600

    build fix test

--- src/pages/schedule/[id].js
+++ src/pages/schedule/[id].js
@@ -23,9 +23,9 @@ export async function getStaticProps({ params }) {
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-  const conferences = await getConferances();
+  const conferences = (await await getConferances()|| [])
   return {
     paths: conferences.map((el) => ({ params: { id: el.id.toString() } })),
-    fallback: true,
+    fallback: false,
   };
 }
